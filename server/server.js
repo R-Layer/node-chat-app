49ba9618b4011f544550166d84528f9fab16eb86
R-Layer <layer@europe.com>
2018-07-02 12:48:20 +0200

    Add connect / disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
+const http = require('http');
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server running at ${port}`))
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => console.log(`Server running at ${port}`))
